ef121ff8c143b3d3c77183b3056da68335143843
SonneyD <41026610+SonneyD@users.noreply.github.com>
2019-07-11 22:46:33 -0700

    Add project files.

---
+++ BungieAPI_Project/Form1.cs
@@ -0,0 +1,32 @@
+﻿using ConsoleGenerator;
+using DestinyEndpints.ClassLibrary;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BungieAPI_Project
+{
+    public partial class Form1 : Form
+    {
+        private DestinyApi api;
+        public Form1()
+        {
+            InitializeComponent();
+            api = new DestinyApi("76de47aca6f44fc48272d98aab6b6e26");
+            
+
+            //var temp = api.Destiny2_SearchDestinyPlayer("SonneyD", 2);
+            Console.WriteLine(api.CommunityContent_GetCommunityContent(1,1,1));
+            
+        }
+    }
+}
